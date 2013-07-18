c93b8f56474674fa8e0b4c3ae334b6f929d6c6d6
Davide De Gennaro <nerevar88@gmail.com>
2013-07-18 18:31:24 +0200

    I got some problems with git ;)

--- bin/mute.py
+++ bin/mute.py
@@ -10,7 +10,7 @@ def main():
     shutil.rmtree('%s/app/assets/sounds' % HOME)
 
     edit = edit_cls('AudioSystemManager')
-    edit.find_line(r' const/4 v0, 0x2')
+    edit.find_line(r' invoke-virtual \{[pv]\d+, [pv]\d+, [pv]\d+\}, Ljava/util/concurrent/atomic/AtomicBoolean;->compareAndSet\(ZZ\)Z')
     edit.find_prologue(where='up')
     edit.prepare_to_insert(2)
     edit.add_line(' return-void')
