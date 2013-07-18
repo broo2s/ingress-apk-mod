591c1f9c55199c61a733f992e45582c6a45ba0ad
Davide De Gennaro <nerevar88@gmail.com>
2013-07-18 18:18:08 +0200

    Revert "Revert "Revert "Fixed mute.py for 1.31.1"""
    This reverts commit cc2cbf0fc0404f1e030bf57aeb95ea7bfa4353b3.

--- bin/mute.py
+++ bin/mute.py
@@ -10,11 +10,7 @@ def main():
     shutil.rmtree('%s/app/assets/sounds' % HOME)
 
     edit = edit_cls('AudioSystemManager')
-<<<<<<< HEAD
     edit.find_line(r' const/4 v0, 0x2')
-=======
-    edit.find_line(r' invoke-virtual \{[pv]\d+, [pv]\d+, [pv]\d+\}, Ljava/util/concurrent/atomic/AtomicBoolean;->compareAndSet\(ZZ\)Z')
->>>>>>> parent of 19ee032... Revert "Fixed mute.py for 1.31.1"
     edit.find_prologue(where='up')
     edit.prepare_to_insert(2)
     edit.add_line(' return-void')
